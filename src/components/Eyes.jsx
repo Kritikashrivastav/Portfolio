dbe6afdec93f9e5c8d1bd6c82c2efad9c6cea495
kritika shrivastav <lumosaccioemail@gmail.com>
2024-10-03 16:59:06 +0530

    eyes effects commit

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -19,7 +19,7 @@ function Eyes() {
 
   return (
     <div className='eyes w-full h-screen mb-20 overflow-hidden'>
-      <div data-scroll data-scroll-speed="-.6" className=' relative w-full bg-contain h-full md:h-[44vw] mt-20 bg-no-repeat bg-center bg-[url("/bg.png")]'>
+      <div data-scroll data-scroll-speed="-.6" className=' relative w-full bg-contain h-full md:h-[44vw] mt-20 bg-no-repeat bg-center bg-[url("/bgbg.png")]'>
         <div className='absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
           <div className='flex items-center justify-center w-[10vw] md:w-[15vw] h-[10vw] md:h-[15vw] rounded-full bg-zinc-100'>
             <div className='relative w-2/3 h-2/3 bg-zinc-900 rounded-full md:rounded-none'>
